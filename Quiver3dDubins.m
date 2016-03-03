d4b9e305b690105515ef9f9221dbcafea827aa3a
peteflorence <pete.florence@gmail.com>
2016-03-03 11:00:27 -0500

    fixed c_1_dot calculation

--- Quiver3dDubins.m
+++ Quiver3dDubins.m
@@ -1,13 +1,14 @@
 
 v = 10;
 
-[c_0,c_1, u] = meshgrid(0.2:0.2:10,-2:0.2:2, -4:0.2:4);
+[c_0,c_1, u] = meshgrid(0.2:0.4:10,-2:0.4:2, -4:0.4:4);
 c_0_dot = -v + c_0 .* c_1 .* u;
-c_1_dot = c_0 .* c_1 .* u * 0 + tan(u);
+c_1_dot = u ./ ( cos(atan(c_1)).^2 );
 u_dot = c_0 .* c_1 .* u * 0;
 
 figure
-quiver3(c_0,c_1,u,c_0_dot,c_1_dot,u_dot); 
+scale = 2;
+quiver3(c_0,c_1,u, scale * c_0_dot, scale * c_1_dot, scale * u_dot); 
 title('Phase plot for all u')
 xlabel('c_0')
 ylabel('c_1')
